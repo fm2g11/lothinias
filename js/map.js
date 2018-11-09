0df3fd0265c85b74b52afdc341f0954c3aae68b0
Francesco <fm2g11@gmail.com>
2018-11-09 17:17:29 +0000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -47,10 +47,9 @@ function getdim(width){
 
 
 function map_controller(){
-    $.get("/lothinias/data/map.json", function(data) {
+    $.get("/data/map.json", function(data) {
         console.log(data);
 //         data = JSON.parse(data);
-//         console.log(data);
         var svg = d3.select('svg');
         var mapDiv = document.getElementById("map-div");
 
